a0fc4b6ee54662dd43b9a0706f41ff9a8682f496
blakrr <blakeyman5@gmail.com>
2024-08-16 17:08:42 -0400

    image adding plus other stuff

--- main.py
+++ main.py
@@ -10,25 +10,30 @@ def main():
     # tk menu stuff
     menu = Menu(tk)
     tk.config(menu=menu)
-    filemenu = Menu(menu)
+    filemenu = Menu(menu, tearoff=False)
+    editmenu = Menu(menu, tearoff=False)
     
     # file menu
     menu.add_cascade(label='File', menu=filemenu)
+    filemenu.add_command(label='Add Song', command= lambda : addSong())
     filemenu.add_separator()
     filemenu.add_command(label='Exit', command=tk.quit)
 
-    # file menu
-    menu.add_cascade(label='File', menu=filemenu)
-    filemenu.add_separator()
-    filemenu.add_command(label='Exit', command=tk.quit)
+    # edit menu
+    menu.add_cascade(label='Edit', menu=editmenu)
+    editmenu.add_command(label='Exit', command=tk.quit)
 
-    #
-    restore_button = Button(tk, text="Minimize", command=tk.withdraw())
-    restore_button.pack()
+    # image button
+    buttonTest = PhotoImage(file='assets/rewind.png')
+    button= Button(tk, image=buttonTest, command= lambda : print('test'), borderwidth=0)
+    button.pack(pady=30)
 
     # tk mainloop
     tk.mainloop()
 
+
+def addSong():
+    print('test')
 
 
 if __name__ == "__main__": # python boilerplate. hooray!
